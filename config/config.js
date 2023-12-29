64b74c4790c1c0e9bd599b12541a296157cea57d
myasir-khan <yasir.khan@aretecinc.com>
2023-12-30 03:50:35 +0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,13 +1,25 @@
 const ca_cert = process.env.ca_cert
 module.exports = {
     development: {
-        username: 'postgres',
-        password: 'postgres',
-        database: 'postgres',
-        host: 'localhost',
-        dialect: 'postgres',
-        port: 5432
+        username: 'doadmin',
+        password: 'AVNS_TV_50hLSrZj13y7Xyc2',
+        database: 'defaultdb',
+        host: 'dbaas-db-2607443-do-user-13996183-0.c.db.ondigitalocean.com',
+        // dialect: 'postgres',
+        port: 25060,
+        sslmode: 'require',
+        ssl: {
+            ca: ca_cert,
+        },
     },
+    // development: {
+    //     username: 'postgres',
+    //     password: 'postgres',
+    //     database: 'postgres',
+    //     host: 'localhost',
+    //     dialect: 'postgres',
+    //     port: 5432
+    // },
     production: {
         username: 'doadmin',
         password: 'AVNS_TV_50hLSrZj13y7Xyc2',
@@ -15,6 +27,7 @@ module.exports = {
         host: 'dbaas-db-2607443-do-user-13996183-0.c.db.ondigitalocean.com',
         // dialect: 'postgres',
         port: 25060,
+        sslmode: 'require',
         ssl: {
             ca: ca_cert,
         },
