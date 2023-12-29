a2a32b7e5d8a510b068fb5c514d96c833cb2c525
myasir-khan <yasir.khan@aretecinc.com>
2023-12-30 03:47:55 +0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -8,11 +8,22 @@ module.exports = {
         port: 5432
     },
     production: {
-        username: 'postgres',
-        password: 'postgres',
-        database: 'postgres',
-        host: process.env.HOST,
-        dialect: 'postgres',
-        port: 5432
+        username: 'doadmin',
+        password: 'AVNS_TV_50hLSrZj13y7Xyc2',
+        database: 'defaultdb',
+        host: 'dbaas-db-2607443-do-user-13996183-0.c.db.ondigitalocean.com',
+        // dialect: 'postgres',
+        port: 25060,
+        ssl: {
+            ca: ca_cert,
+        },
     }
+    // production: {
+    //     username: 'postgres',
+    //     password: 'postgres',
+    //     database: 'postgres',
+    //     host: process.env.HOST,
+    //     dialect: 'postgres',
+    //     port: 5432
+    // }
 }
